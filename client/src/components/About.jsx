2299cfb3732bc43565da0cf5eda75d5e51357105
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-07 13:55:22 -0600

    update

---
+++ client/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+export default class About extends PureComponent {
+  render () {
+    return (
+      <div className="inner cover">
+        <h1 className="cover-heading">Javascript Everywhere</h1>
+        <p className="lead">Test paragraph. Trying to learn react</p>
+      </div>
+    );
+  }
+}
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import '../dist/css/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+
+filepicker.setKey("AWSLFdDFYSm6VCFYa3ArEz");
+
+ReactDOM.render(
+  Routes,
+  document.getElementById('content')
+);
