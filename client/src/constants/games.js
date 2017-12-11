3eead811273da160f98fb5562aa0a4b7b8693287
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-11 13:22:48 -0600

    update

---
+++ client/src/constants/games.js
@@ -0,0 +1,10 @@
+/* the constants are imported in the sagas, reducers and action files so it's very convenient to have a 'centralized' file for them. */
+const GET_GAMES = 'GET_GAMES';
+const GET_GAMES_SUCCESS = 'GET_GAMES_SUCCESS';
+const GET_GAMES_FAILURE = 'GET_GAMES_FAILURE';
+
+export {
+  GET_GAMES,
+  GET_GAMES_SUCCESS,
+  GET_GAMES_FAILURE
+};
---
+++ client/src/reducers/games.js
@@ -0,0 +1,25 @@
+import Immutable from 'immutable';
+// Here the constants file comes handy
+import {
+  GET_GAMES_SUCCESS,
+  GET_GAMES_FAILURE
+} from '../constants/games';
+
+// The initial state is just an empty Map
+const initialState = Immutable.Map();
+
+// That's a very standard reducer function to return a new state given a dispatched action
+export default (state = initialState, action) =&gt; {
+  switch (action.type) {
+  // GET_GAMES_SUCCESS case return a new state with the fetched games in the state
+    case GET_GAMES_SUCCESS: {
+      return state.merge({ list: action.games });
+    }
+  // In case of failure it simplies returned a new empty state
+    case GET_GAMES_FAILURE: {
+      return state.clear();
+    }
+    default:
+      return state;
+  }
+}
