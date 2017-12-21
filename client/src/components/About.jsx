d4252b155eebba34cf733e66dfd388e39f2a594c
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-21 11:06:41 -0600

    update

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,7 +4,7 @@ export default class About extends PureComponent {
   render () {
     return (
       <div className="inner cover">
-        <h1 className="cover-heading">Javascript Everywhere</h1>
+        <h1 className="cover-heading">Old School Games</h1>
         <p className="lead">Test paragraph. Trying to learn react</p>
       </div>
     );
